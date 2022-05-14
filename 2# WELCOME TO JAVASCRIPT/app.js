c7d4250e4fa0557bcdf83a45fa65a7c7807d37a7
taewan625 <taewan1997625@gmail.com>
2022-05-14 22:01:00 +0900

    #2.2 Variables

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,1 +1,24 @@
-// 경고창을 띄우기싫어서 지움
+console.log(); // console에 log or print 하는 역할 (숫자 or  문자)
+
+// variable : js + 모든 언어에서 값을 저장하거나 유지하는 역할을 함
+
+const a = 6;
+const b = 5;
+
+//바뀌지 않음을 뜻함. variable을 만들기 위해 필요. 상수라는 뜻으로 바뀌지 않은 값을 의미
+// a : constant인 variable의 이름
+// a의 이름을 가진 바뀌지 않는 variable은 5와 같다
+
+console.log(a * b);
+console.log(a + b);
+console.log(a / b);
+
+// a의 숫자만 바꾸면 되므로 게을러질수 있다~
+// 게으른 개발자 = code 적게 쓰는 개발자는 code가 적어서 시간도 절약되고 컴퓨터 오류도 적다
+
+const myName = "taewan";
+//  js variable의 name에는 공백이 없어야 하고 만약 필요하면 capital을 사용해야 한다     = camelCase
+// * python에서는 대문자를 사용하지 않고 my_name 으로 _ 이용 capital을 사용하지 않는다. = sanke_case
+//   +a ) python variable은 const를 안붙이고 my_name = "taewan" 이렇게 한다
+
+console.log("hello my name is", myName);
