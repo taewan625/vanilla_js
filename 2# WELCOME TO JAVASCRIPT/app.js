b36e98f83fa32bc05a28d0a79b1d8a3ab02c6129
taewan625 <taewan1997625@gmail.com>
2022-05-15 14:13:43 +0900

    #2.4 Booleans

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,27 +1,16 @@
-const a = 6;
-const b = 5;
-let myName = "taewan";
+// variable 안에 들어갈 수 있는 숫자 or string 말고 넣을 수 있는 datatyle - bopleans
+// Boolean 은 True or False 을 의미 ex) login 했는가 안했는가 / 동영상 재생되는가 아닌가
 
-// JS에는 variable을 만드는 방법이 let 과 constant 2개 존재
-//l let과 constant 차이점 : constant는 상수이고 값이 바뀔 수 없다
+// true or false          python : True or False
+const amIFat = true;
+console.log(amIFat);
 
-// variable 값을 바꾸고 싶은 경우 : let 을 이용하는 것인데 let은 처음에만 variable을 지정해주면 다시 쓸 필요 없다
+//null : 값이 없을 때 사용    null false와는 값이 다르다   pythone : none
+const amIFat1 = null;
+console.log(amIFat1);
 
-console.log(a * b);
-console.log(a + b);
-console.log(a / b);
+// undefined : null은 값이 없다라고 뭔가를 채워준 defined 된것 but 얘는 채워지지도 않음
 
-console.log("hello my name is", myName);
-
-myName = "kwon taewan";
-console.log("hello my new name is", myName);
-
-// 처음 myname의 값이 let을 사용하지 않고 constant 를 이용하면 다음에 update 시 error가 생김
-
-// JS에서의 장점은 const 와 let이 존재하여 값이 update 되는지 아닌지 확인할 수 있는 장점이 있고
-// 보통 constant 를 사용하다가 필요시 let을 사용한다
-
-// JS는 너무 대충 만든거라 update가 안되고 계속 보완하는 형식으로 진행이 된다. 그래서 const와 let 도 최근에 나온거고
-// 옛날 JS 책에선 var이라고 가르치는데 이는 contant와 반대로 실수롤 값을 바꿔도 let 처럼 ok 하고 넘어간다.
-// 그래서 JS는 최신 것을 계속 공부해야 따라갈수 있다
-// var은 절대 쓰지 말 것
+let something;
+console.log(something);
+// console 창에 undefined라고 정의 됨
