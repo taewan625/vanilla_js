821d25dfa2abfe954923b8ae9ef782af948965b5
taewan625 <taewan1997625@gmail.com>
2022-05-15 15:05:02 +0900

    #2.5Arrays

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,16 +1,26 @@
-// variable 안에 들어갈 수 있는 숫자 or string 말고 넣을 수 있는 datatyle - bopleans
-// Boolean 은 True or False 을 의미 ex) login 했는가 안했는가 / 동영상 재생되는가 아닌가
+// data 정리하는 법
+// 가장 기본적인 data structure는 array 이다
 
-// true or false          python : True or False
-const amIFat = true;
-console.log(amIFat);
+// array 가 없는 요일 작성법
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-//null : 값이 없을 때 사용    null false와는 값이 다르다   pythone : none
-const amIFat1 = null;
-console.log(amIFat1);
+// list로 그룹화
 
-// undefined : null은 값이 없다라고 뭔가를 채워준 defined 된것 but 얘는 채워지지도 않음
+let daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+console.log(daysOfWeek);
+// 엄청 긴게 나옴
 
-let something;
-console.log(something);
-// console 창에 undefined라고 정의 됨
+// array
+daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+// 현재 mon, tue, wed 들은 " " 안에 있지 않아서 valiable로 취급 되고 "" 넣어주면 text로 바뀜
+console.log(daysOfWeek);
+// [] 이게 array를 위한 기호고 ,로 나눠줌어느 datatype이든지 다 들어감
+
+const anyDataType = [true, false, null, undefined, 123, "taewan"];
+console.log(anyDataType);
