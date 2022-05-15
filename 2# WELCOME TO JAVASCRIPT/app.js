5ad43f0ea39d8f5fb7dd518a4b2132b4e37ec1c6
taewan625 <taewan1997625@gmail.com>
2022-05-15 16:38:36 +0900

    #2.6 Objects

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,16 +1,41 @@
-// data 정리하는 법
-// 가장 기본적인 data structure는 array 이다
+// const playerName = "taewan";
+// const playerPoints = 12341241;
+// const playerHandsome = true;
+// const playerFat = "little bit";
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-//              0      1      2      3      4      5      6
+// const player = ["taewan", 12341241, true, "little bit"];
+// array는 각각의 값이 무엇을 의미하는지 알려주지 않음
+// player[0] == name player[1] ==points
 
-// list에서 필요한 부분을 가져오는 것_금요일
-console.log(daysOfWeek);
+// const player
+// player.name;
+// player.points;
+// player.handsome;
 
-// Add one more day to the array
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
-// sun이 추가 됨
+//  object : { }; 사용, { } 안에는 = 아닌 : 사용
+const player = {
+  name: "taewan",
+  points: 10,
+  fat: true,
+  handsome: true,
+};
+console.log(player);
+console.log(player.name);
+// player.name을 보고 알 수 있는 것 : player라는 object 안에 있는 name이란 property를 가져온 것
+// 마찬가지로 console.log 또한      : console이란 object 안에 있는 log이란 property를 가져온 것
+console.log(player["name"]); //이렇게도 가능
 
-const toBuy = ["potato", "tomato", "apple"];
-toBuy.push("kimbab");
+//update 가능
+player.fat = false;
+// 이건 edit의 개념이라서 const 가능한거고 object의 여러 값은 하나로 바꾸거나, dataType을 바꿀 때 error 나옴
+player.points = player.points + 15;
+player.name = player.name + "kwon";
+// 원래 가지고 있는 값에 추가가 가능함 ㄷㄷ
+console.log(player);
+
+//property add
+player.lastName = "kwon";
+console.log(player);
+
+// 정리 [list];   : 모든 값이 같은 의미를 가짐
+//      {object}; : 값이 각각 다른 value를 가짐
