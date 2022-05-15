51b5e509eb3ba7595ed847b650c12e8b4142955c
taewan625 <taewan1997625@gmail.com>
2022-05-15 16:01:21 +0900

    Update1 #2.5 Arrays

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,7 +1,16 @@
 // data 정리하는 법
 // 가장 기본적인 data structure는 array 이다
 
-daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 //              0      1      2      3      4      5      6
+
 // list에서 필요한 부분을 가져오는 것_금요일
-console.log(daysOfWeek[4]);
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+// sun이 추가 됨
+
+const toBuy = ["potato", "tomato", "apple"];
+toBuy.push("kimbab");
