49590a712c0b4ea8e96ed3439824e4c40145a544
taewan625 <taewan1997625@gmail.com>
2022-05-18 08:33:29 +0900

    #2.7 Function part One

--- 2# WELCOME TO JAVASCRIPT/app.js
+++ 2# WELCOME TO JAVASCRIPT/app.js
@@ -1,41 +1,22 @@
-// const playerName = "taewan";
-// const playerPoints = 12341241;
-// const playerHandsome = true;
-// const playerFat = "little bit";
+// console.name() : name은 ()가 불가능. function이 아니기 때문
+// console.log()  ; 얘는 ()가 가능하다. log가 function이기 때문
 
-// const player = ["taewan", 12341241, true, "little bit"];
-// array는 각각의 값이 무엇을 의미하는지 알려주지 않음
-// player[0] == name player[1] ==points
+//  function ; 계속 반복해서 사용할 수 있는 코드 조각이라고 생각하면 된다
 
-// const player
-// player.name;
-// player.points;
-// player.handsome;
+// function이 없으면 이렇게...
+// console.log("Hello my name is a");
+// console.log("Hello my name is b");
+// console.log("Hello my name is c");
+// console.log("Hello my name is d");
+// console.log("Hello my name is e");
 
-//  object : { }; 사용, { } 안에는 = 아닌 : 사용
-const player = {
-  name: "taewan",
-  points: 10,
-  fat: true,
-  handsome: true,
-};
-console.log(player);
-console.log(player.name);
-// player.name을 보고 알 수 있는 것 : player라는 object 안에 있는 name이란 property를 가져온 것
-// 마찬가지로 console.log 또한      : console이란 object 안에 있는 log이란 property를 가져온 것
-console.log(player["name"]); //이렇게도 가능
+//function 이용
+// function + function name + () + { 여기에 반복하고 싶은 것 넣음 ; }
+// argument : function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법
+//
+function sayHello() {
+  console.log("Hello my name is c");
+}
+sayHello("a");
 
-//update 가능
-player.fat = false;
-// 이건 edit의 개념이라서 const 가능한거고 object의 여러 값은 하나로 바꾸거나, dataType을 바꿀 때 error 나옴
-player.points = player.points + 15;
-player.name = player.name + "kwon";
-// 원래 가지고 있는 값에 추가가 가능함 ㄷㄷ
-console.log(player);
-
-//property add
-player.lastName = "kwon";
-console.log(player);
-
-// 정리 [list];   : 모든 값이 같은 의미를 가짐
-//      {object}; : 값이 각각 다른 value를 가짐
+// 현재 function을 만들고 이를 출력하는 것까지는 알고 있다. 하지만 function의 c라는 부분에 원하는 data를 바꾸는 법을 알아야 한다 = argument(인수)를 보내는 것
