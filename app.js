49dfb5794e952eb796d47757adb3767478300ca5
taewan625 <taewan1997625@gmail.com>
2022-05-24 10:12:40 +0900

    #3.1 The Document Object

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// === 와 == 차이
-// === : 유형까지 꼼꼼히 따짐 == : 비슷하게 생기면 상관 없음 True == 1  False == 0 의 default 값을 가짐
-// https://developer-talk.tistory.com/184
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real posotive number");
-} else if (age < 18) {
-  console.log("Please get out");
-} else if (age >= 18 && age <= 50) {
-  console.log("Welcome to bar");
-} else if (age > 50 && age <= 80) {
-  console.log("You can drink but careful");
-} else if (age === 100) {
-  console.log("wow you are wise");
-  // 아닐 때는 !==로 표현
-} else if (age > 80) {
-  console.log("You can do whatever you want");
-}
+// browser console 에 document를 입력하면 현재 연결된 html file을 보여준다.  * 더 자세한 것은 console.dir(document) 입력
+// document: browser에 이미 존재하는 object이다 *html로 접근할 수 있는 객체
+// object 관점으로 보여주는 것이 중요한 이유는 document object로부터 title을 가지고 올 수 있기 때문
+// document.title 은 현재 html title을 보여주는 것이고 document.title = "###"로 title을 변경이 가능하다 -> html과 JS 는 상호호환되어 있다
+document.title = "momentum_can_change";
