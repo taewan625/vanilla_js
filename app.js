ec9685d788e4ef5f2ddb7ab041761bf882bfb7c7
taewan625 <104051002+taewan625@users.noreply.github.com>
2022-05-26 09:51:34 +0900

    #3.1 The Document Object

--- app.js
+++ app.js
@@ -3,3 +3,27 @@
 // object 관점으로 보여주는 것이 중요한 이유는 document object로부터 title을 가지고 올 수 있기 때문
 // document.title 은 현재 html title을 보여주는 것이고 document.title = "###"로 title을 변경이 가능하다 -> html과 JS 는 상호호환되어 있다
 document.title = "momentum_can_change";
+
+//mmgl com
+// browser console 에 document를 입력하면 현재 연결된 html file을 보여준다.  * 더 자세한 것은 console.dir(document) 입력
+// document: browser에 이미 존재하는 object이다 *html로 접근할 수 있는 객체
+// object 관점으로 보여주는 것이 중요한 이유는 document object로부터 title을 가지고 올 수 있기 때문
+// document.title 은 현재 html title을 보여주는 것이고 document.title = "###"로 title을 변경이 가능하다 -> html과 JS 는 상호호환되어 있다
+const title = document.getElementById("titleeeeeeeeeeee");
+// console.dir(title);
+
+title.innerText="Got you!!" // js를 이용해서 html file을 안건들이고 변경이 가능하다
+
+
+//JS로 title의 id와 class 이름을 확인하는 방법
+console.log(title.id);
+
+console.log(title.className);
+
+
+//  html에서 항목들을 가지고 와서, JS를 통해 항목들을 변경할 것
+// 개념이 중요: JS로 html 요소들을 찾아내면 그 후 JS를 이용해서 변경이 가능하다
+
+// html에서 항목들을 가지고 올수 있는 방법 2가지
+// 1) document에서 항목들을 가지고 오는 것
+// 2) document 항목들을 변경하는 것
