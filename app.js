4c4faa537add8e7df4a0a3ac44e4fcb8a5d6a2f2
taewan625 <taewan1997625@gmail.com>
2022-05-31 08:58:19 +0900

    #3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const h1 = document.querySelector(".hello h1");
 
+// Console 창에서 h1의 style의 color를 확인하는 방법
+// function handleh1Click() {
+//   console.log(h1.style.color); // 함수안의 내용은 h1에 관한 것
+//   h1.style.color = "blue";
+//   console.log(h1.style.color);
+// }
+
+// function handleh1Click() {
+//   if (h1.style.color === "blue") {
+//     // === 동일한지 알아보는 것
+//     h1.style.color = "tomato";
+//   } else {
+//     h1.style.color = "blue"; // css와 달리 JS는 함수안의 값이 존재하는 element가 아닌 이상 ""을 이용하는 것 같다
+//   }
+// }
+
 function handleh1Click() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor; // 안에 값이 존재하지 않게 하므로 이 상태로 닫음
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor; // newColor라는 것은 값을 가지는 element같은 거라 ""없이 사용이 가능한 것 같음
 }
+// 해석 : 시작은 black color 이므로 h1.style.color = black이 되고 black은 else로 넘어가서 newColor="blue" 작동시킴 그리고 마지막으로 원래 newColor="blue"에서는 아무러 작동이 없음
+// 왜냐면 newColor라는 애가 아무런 html 요소들과 연결이 되어있지 않기 때문 하지만 h1.style.color = newColor; 덕분에 color의 변화가 생김
+h1.addEventListener("click", handleh1Click);
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-  // document는 html을 뜻함. 그래서 나중에 document.title을 하면 h1의 내용이 아니라 head의 title을 가리키므로 title을 h1을 변경함
-  // body, head, title은 중요하기 때문에 document.title 는 가능하지만 다른 element들은 1줄 처럼 querySelector를 이용하거나 getElementById를 이용해야함
-}
-
-function handleWindowCopy() {
-  alert("coper!!");
-}
-
-function handleWindowOffline() {
-  alert("404Error_noWifi");
-}
-
-function handleWindowOnline() {
-  alert("connectedWifi");
-}
-// h1.addEventListener 은 나중에 h1.removeEventListener로 제거 가능해서 추천
-h1.addEventListener("click", handleh1Click); // smae : h1.onclick = handleh1Click;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-// window 관련 event들
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+// 원래 JS로 css 속성 바꾸는 것은 나중에 사람 헷갈리게 해서 비추천
+// HTML은 HTML 자체로 변화하는 방법이 없어서 어쩔수 없이 JS를 이용해야 함
+// 1. element를 찾아라  2. event를 listen 하라 3. event에 반응(보여주기 감추기 색 바꾸기)해라
