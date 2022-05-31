ed70c857196aefc8057c3768c1d89d6798f0074d
taewan625 <taewan1997625@gmail.com>
2022-05-31 18:21:08 +0900

    # my mistake

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // classList _ i control class seperatery very cool     developer.mozilla.org
 // className : change everything, doesnt care about before class
-
 // const h1 = document.querySelector(".hello h1");
 // // This function is basic one
 // function handleh1Click() {
@@ -12,12 +11,11 @@
 //     h1.classList.add(clickedClass); // else time, add clickedClass's class
 //   }
 // }
-// h1.addEventListener("click", handleh1Click);
 
+// h1.addEventListener("click", handleh1Click);
 // upgrade function toggle!!
 // toggle which inside classList function : check class name -> if classname exist toggle remove classnmae else toggle add classname
 const h1 = document.querySelector(".hello h1");
-
 function handleh1Click() {
   // const clickedClass = "clicked";     // -- we only use one classname so no need make const
   // h1.classList.toggle(clickedClass);
