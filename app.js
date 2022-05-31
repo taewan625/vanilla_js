3502414e6989f43a950f8d2d2b9e8c5b7d6196b9
taewan625 <taewan1997625@gmail.com>
2022-05-31 10:01:31 +0900

    #3.8 CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
+// 앞의 강의에서 하는 방식으로 JS를 style하는 방식은 가능하지만 스마트하지 않다
+// style에 적합한 방식은 CSS 이고 JS에 적합한 방식은 animation이다.
+
 const h1 = document.querySelector(".hello h1");
 
-// Console 창에서 h1의 style의 color를 확인하는 방법
 // function handleh1Click() {
-//   console.log(h1.style.color); // 함수안의 내용은 h1에 관한 것
-//   h1.style.color = "blue";
-//   console.log(h1.style.color);
+//   h1.className = "active";
 // }
-
-// function handleh1Click() {
-//   if (h1.style.color === "blue") {
-//     // === 동일한지 알아보는 것
-//     h1.style.color = "tomato";
-//   } else {
-//     h1.style.color = "blue"; // css와 달리 JS는 함수안의 값이 존재하는 element가 아닌 이상 ""을 이용하는 것 같다
-//   }
-// }
+// // h1 html에 보면 active란 class가 없지만 JS를 이용해서 위의 function을 이용해서 JS와 css를 연결함
+// // 그래서 console 창에서 보면 h1 class=active가 나오고 css 에서 .active는 color를 tomato로 바꾸는 역할을 해줌
 
 function handleh1Click() {
-  const currentColor = h1.style.color;
-  let newColor; // 안에 값이 존재하지 않게 하므로 이 상태로 닫음
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked font"; // font의 class를 추가해주는 것도 그렇게 좋은 방법은 아니다
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor; // newColor라는 것은 값을 가지는 element같은 거라 ""없이 사용이 가능한 것 같음
+  // clicked라는 string을 2번 사용하므로써 오류를 범할 가능성이 증가한다 -> 왜냐면 clickedfmf clikced로 철자 틀리고 css class 명을 바꿀 때 또 일일히 바꿔주면 실수 가능성이 높음
+  //
 }
-// 해석 : 시작은 black color 이므로 h1.style.color = black이 되고 black은 else로 넘어가서 newColor="blue" 작동시킴 그리고 마지막으로 원래 newColor="blue"에서는 아무러 작동이 없음
-// 왜냐면 newColor라는 애가 아무런 html 요소들과 연결이 되어있지 않기 때문 하지만 h1.style.color = newColor; 덕분에 color의 변화가 생김
 h1.addEventListener("click", handleh1Click);
 
-// 원래 JS로 css 속성 바꾸는 것은 나중에 사람 헷갈리게 해서 비추천
-// HTML은 HTML 자체로 변화하는 방법이 없어서 어쩔수 없이 JS를 이용해야 함
-// 1. element를 찾아라  2. event를 listen 하라 3. event에 반응(보여주기 감추기 색 바꾸기)해라
+// 하지만 이 방법은 css h1 안에 다른 class를 넣을 때 그것들을 다 없애버리는 문제점이 있음
