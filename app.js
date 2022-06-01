b12c51c09989500d9add1cef03bcd7fe4cd8feac
taewan625 <taewan1997625@gmail.com>
2022-06-01 15:51:07 +0900

    #4.0 Input Values

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-// classList _ i control class seperatery very cool     developer.mozilla.org
-// className : change everything, doesnt care about before class
-// const h1 = document.querySelector(".hello h1");
-// // This function is basic one
-// function handleh1Click() {
-//   const clickedClass = "clicked";
-//   if (h1.classList.contains(clickedClass)) {
-//     // classList have property like contains, remove, add . so this mean only thinking about clickedClass
-//     h1.classList.remove(clickedClass); // if clickedClass is true, remove only clickedClass's class
-//   } else {
-//     h1.classList.add(clickedClass); // else time, add clickedClass's class
-//   }
-// }
+// 현재 하고 있는 것은 HTML를 찾는 것
+// 1) div를 찾음
+const loginForm = document.getElementById("login-form");
+// const loginForm = document.querySelector("#login-form"); // querySelector은 모든 것을 다 끌고 오므로 #나 . 붙여줘야 함
+// 2) input과 button 찾기
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-// h1.addEventListener("click", handleh1Click);
-// upgrade function toggle!!
-// toggle which inside classList function : check class name -> if classname exist toggle remove classnmae else toggle add classname
-const h1 = document.querySelector(".hello h1");
-function handleh1Click() {
-  // const clickedClass = "clicked";     // -- we only use one classname so no need make const
-  // h1.classList.toggle(clickedClass);
-  h1.classList.toggle("clicked");
+// // 다른 방법
+// const loginInput = document.querySelector("#login-form input")
+// const loginButton = document.querySelector("login-form button")
+
+function onLoginBtnClick() {
+  console.log(loginInput.value); // console.dir(loginInput); -> 이것을 통해서 value라는 위치를 찾은 것임
+  // console.log("clicked");
 }
-h1.addEventListener("click", handleh1Click);
+loginButton.addEventListener("click", onLoginBtnClick);
+// console.dir로 loginInput 내부에 내가 친 내용들이 value라는 값을 가짐을 알 수 있다.
+// 이는 html input 안에 value="###"" 하면 값이 선 입력됨을 의미하기도 한다.
