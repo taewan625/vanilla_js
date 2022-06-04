c59b23ab734c93cb4b4546834492e8bfa869830d
taewan625 <taewan1997625@gmail.com>
2022-06-04 10:56:20 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting"); // html h1 안에 greeting이란 id 넣고 변수 만듬
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; // 대문자로 하게 변수명을 지정하는 것은 관습적인 것, 그리고 별 중요하지 않은 것을 나타날때 사용
 
 function onLoginSubmit(event) {
-  event.preventDefault(); // 어떤 function에 대해 browser가 기본적으로 수행하는 동작 을 하지 않도록 막아주는 역할
-  // 받은 정보
-  // console.log(event);
-  //   SubmitEvent {isTrusted: true, submitter: input, type: 'submit', target: form#login-form, currentTarget: form#login-form, …}
-  // isTrusted: true
-  // bubbles: true
-  // cancelBubble: false
-  // cancelable: true
-  // composed: false
-  // currentTarget: null
-  // defaultPrevented: true
-  // eventPhase: 0
-  // path: (5) [form#login-form, body, html, document, Window]
-  // returnValue: false
-  // srcElement: form#login-form
-  // submitter: input
-  // target: form#login-form
-  // timeStamp: 1823.1000000238419
-  // type: "submit"
-  // [[Prototype]]: SubmitEvent
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
   event.preventDefault();
-  console.dir(event);
-  // console.log(event);
-  // // 받은 정보
-  // // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-  // alert("clicked");
+  loginForm.classList.add(HIDDEN_CLASSNAME); // hidden이란 class name을 더해줘서 form을 숨김
+  const username = loginInput.value; // user이름을 변수로 저장해서 그 이름을 h1 안에 넣을 것임
+
+  // greeting.innerText = "hello" + username;
+  greeting.innerText = `hello ${username}`; // 둘다 변수를 string 안에서 사용할 수 있게 해줌
+
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
-
-// EventListener에서 handleLinkClick이란 함수만 실행하는 것이 아니라 함수 실행 명령어인 () 안에 argument를 {object} 를 받게 되면서
-//  이 object가 방금 일어난 event에 대한 여러 정보를 나타내준다.
-// handleLinkClick({information about the event that just happened})
