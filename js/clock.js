6f421186a1bb677c2e6883716101c85f4166279d
taewan625 <taewan1997625@gmail.com>
2022-06-06 17:59:27 +0900

    #5.1 Timeout and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,26 @@
-// setTimer
-const clock = document.querySelector("#clock");
+// Instance methods
+// new Date()
+// Mon Jun 06 2022 17:36:34 GMT+0900 (한국 표준시)
 
-function sayHello() {
-  console.log("hello");
+//
+// undefined
+
+// const date = new Date();  이렇게 변수명을 지정해주는 것이 중요
+// undefined
+
+// date.getDate()
+// 6
+
+// date.getDay()
+// 1
+
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; // html 로 나오게 clock(#id)의 innerText 이용
 }
 
-setTimeout(sayHello, 3000);
-// setInterval과 다른 점은 바로 function이 작동하지 않고 지정한 시긴을 지난 후 작동
+getClock(); // reload 시 1초 안기다리고 바로 나오게 하려고 넣음
+setInterval(getClock, 1000);
