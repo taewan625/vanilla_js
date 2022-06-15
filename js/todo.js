72b23bf8d532242460f96885a3fa38f3cae6fdb7
taewan625 <taewan1997625@gmail.com>
2022-06-15 10:17:17 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -6,8 +6,8 @@ const toDoInput = document.querySelector("#todo-form input"); // = toDoForm.quer
 function handleToDoSubmit(event) {
   event.preventDefault();
   // console.log(toDoInput.value);
-  const newTodo = toDoInput.value;
-  toDoInput.value = "";
+  const newTodo = toDoInput.value; // input 내용이 비워지기전에 값을 저장해주는 역할
+  toDoInput.value = ""; // input에 무엇인가를 넣었을 때 다시 비워주는 역할
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
