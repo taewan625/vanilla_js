81e35ff02bd081bd9722ac54c1075977a44b2e58
taewan625 <taewan1997625@gmail.com>
2022-06-15 10:48:03 +0900

    #7.1 Adding ToDos
    form 안에 값을 넣었을 때 todo list가 나올 수 있게 하기까지
    F5 할 때 사라짐과 완료한 todolist 없애는 것은 #7.2 때 설명

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,23 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
-
 const toDoInput = document.querySelector("#todo-form input"); // = toDoForm.querySelector("input");
+
+function paintToDo(newTodo) {
+  // 2) 아래 function인 newTodo에서 저장한 value를 이용해서 function을 수행하고
+  // console.log("i will paint", newTodo); // 잘 작동하는지 확인용
+  const li = document.createElement("li"); // 변수명은 굳이 html li로 할 필요는 없지만 createElement는 li(html tag)로 필히 나타내야함
+  const span = document.createElement("span"); // todo list 이므로 span 안에 li를 넣고 그안 span에 리스트를 넣고 button도 만들어서 지우는 역할도 수행할 것
+  li.appendChild(span); // 단계별로보면 toDoInput > li > span = newTodo
+  span.innerText = newTodo;
+  // console.log(li); // 확인용
+  toDoList.appendChild(li); // 이걸로 ul 안에 li 만들어 넣어서 계속 나올수 있게 함
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  // console.log(toDoInput.value);
-  const newTodo = toDoInput.value; // input 내용이 비워지기전에 값을 저장해주는 역할
-  toDoInput.value = ""; // input에 무엇인가를 넣었을 때 다시 비워주는 역할
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo); // 1) 실제로 paintToDo function을 수행하기 위한 코드
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
